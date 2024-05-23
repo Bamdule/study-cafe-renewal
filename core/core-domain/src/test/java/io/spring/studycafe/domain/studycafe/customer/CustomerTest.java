eec28a240c9f3d4138dd65367b30c9c58c861156
bamdule <bamdule5@gmail.com>
2024-05-23 17:56:14 +0900

    test : 고객 도메인 테스트

--- core/core-domain/src/test/java/io/spring/studycafe/domain/studycafe/customer/CustomerTest.java
+++ core/core-domain/src/test/java/io/spring/studycafe/domain/studycafe/customer/CustomerTest.java
@@ -2,6 +2,7 @@ package io.spring.studycafe.domain.studycafe.customer;
 
 import io.spring.studycafe.domain.common.TimeInfo;
 import io.spring.studycafe.domain.studycafe.customerticket.CustomerTicket;
+import io.spring.studycafe.domain.studycafe.ticket.Ticket;
 import io.spring.studycafe.domain.studycafe.ticket.TicketType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,39 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 class CustomerTest {
+
+    @Test
+    void 고객_생성_테스트() {
+        Customer customer = createNoTicketCustomer(1L, 1L, 1L);
+
+        Assertions.assertThat(customer.getId()).isNotNull();
+        Assertions.assertThat(customer.getMemberId()).isNotNull();
+        Assertions.assertThat(customer.getStudyCafeId()).isNotNull();
+        Assertions.assertThat(customer.hasNotTicket()).isTrue();
+    }
+
+    @Test
+    void 고객_이용권_갱신_테스트() {
+        Customer customer = new Customer(1L, 1L);
+
+        Ticket ticket = new Ticket(
+            1L,
+            "테스트 시간 티켓",
+            TicketType.TIME,
+            new TimeInfo(30, 0, 0),
+            120000,
+            30,
+            1L,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        );
+
+        customer.updateTicket(ticket);
+
+        Assertions.assertThat(customer.getMemberId()).isNotNull();
+        Assertions.assertThat(customer.getStudyCafeId()).isNotNull();
+        Assertions.assertThat(customer.hasTicket()).isTrue();
+    }
 
     @Test
     public void 고객_티켓_시간_차감_테스트() {
@@ -32,5 +66,47 @@ class CustomerTest {
         Assertions.assertThat(customer.getCustomerTicket().getTimeInfo()).isEqualTo(new TimeInfo(0, 5, 9));
     }
 
+
+    private static Customer createCustomer(Long customerId, Long memberId, Long studyCafeId) {
+
+        CustomerTicket customerTicket = new CustomerTicket(
+            customerId,
+            TicketType.TIME,
+            new TimeInfo(1, 1, 1),
+            LocalDate.now().plusDays(30),
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        );
+
+        return new Customer(
+            customerId,
+            memberId,
+            studyCafeId,
+            customerTicket,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        );
+    }
+
+    private static Customer createNoTicketCustomer(Long customerId, Long memberId, Long studyCafeId) {
+
+        CustomerTicket customerTicket = new CustomerTicket(
+            customerId,
+            TicketType.NONE,
+            new TimeInfo(0, 0, 0),
+            null,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        );
+
+        return new Customer(
+            customerId,
+            memberId,
+            studyCafeId,
+            customerTicket,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        );
+    }
 
 }
